af86b2d442f3b8e1333b6baa97680c929b586b8e
Lumith226 <thang124365@gmail.com>
2024-11-04 23:31:21 +0700

    Create Settings.java

---
+++ src/main/java/btl/weather/Settings.java
@@ -0,0 +1,41 @@
+public class Settings {
+    private String temperatureUnit;
+    private String windSpeedUnit;
+    private String language;
+
+    public Settings() {
+        this.temperatureUnit = "Celsius";
+        this.windSpeedUnit = "m/s";
+        this.language = "English";
+    }
+
+    public Settings(String tUnit, String wsUnit, String language) {
+        this.temperatureUnit = tUnit;
+        this.windSpeedUnit = wsUnit;
+        this.language = language;
+    }
+
+    public String getTemperatureUnit() {
+        return this.temperatureUnit;
+    }
+
+    public void setTemperatureUnit(String unit) {
+        this.temperatureUnit = unit;
+    }
+
+    public String getWindSpeedUnit() {
+        return this.windSpeedUnit;
+    }
+
+    public void setWindSpeedUnit(String unit) {
+        this.windSpeedUnit = unit;
+    }
+
+    public String getLanguage() {
+        return this.language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+}
