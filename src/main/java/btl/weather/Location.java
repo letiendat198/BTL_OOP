ec20f772b6618df385a7f3f6c3fa374670cd4f85
Le Tien Dat <le.tien.dat.198@gmail.com>
2024-12-02 21:57:54 +0700

    Update Location

--- src/main/java/btl/weather/Location.java
+++ src/main/java/btl/weather/Location.java
@@ -3,42 +3,17 @@ package btl.weather;
 import java.io.Serializable;
 
 public class Location implements Serializable {
-    private double longitude;
-    private double latitude;
     private String city;
     private String country;
-    private float timezone;
 
     public Location() {
-        this.longitude = 0.0;
-        this.latitude = 0.0;
         this.city = "";
         this.country = "";
-        this.timezone = 0.0f;
     }
 
-    public Location(double longitude, double latitude, String city, String country, float timezone) {
-        this.longitude = longitude;
-        this.latitude = latitude;
+    public Location(String city, String country) {
         this.city = city;
         this.country = country;
-        this.timezone = timezone;
-    }
-
-    public double getLongitude() {
-        return this.longitude;
-    }
-
-    public void setLongitude(double longitude) {
-        this.longitude = longitude;
-    }
-
-    public double getLatitude() {
-        return this.latitude;
-    }
-
-    public void setLatitude(double latitude) {
-        this.latitude = latitude;
     }
 
     public String getCity() {
@@ -55,13 +30,5 @@ public class Location implements Serializable {
 
     public void setCountry(String country) {
         this.country = country;
-    }
-
-    public float getTimezone() {
-        return this.timezone;
-    }
-
-    public void setTimezone(float timezone) {
-        this.timezone = timezone;
     }
 }
